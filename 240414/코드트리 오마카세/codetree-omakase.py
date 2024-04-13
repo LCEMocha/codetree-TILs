0f13dde1b36d0a6a1f6ccab0c76275e1715d76c3
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-04-14 01:21:29 +0900

    Title: 코드트리 오마카세, Time: 1235ms, Memory: 93MB, Status: Passed - CodeTree

--- 240414/코드트리 오마카세/codetree-omakase.py
+++ 240414/코드트리 오마카세/codetree-omakase.py
@@ -1,98 +1,112 @@
-import collections
-import copy
+# 변수 선언
+L, Q = 0, 0
 
-table = collections.defaultdict(list)
-customer_remain = {}
+class Query:
+    def __init__(self, cmd, t, x, name, n):
+        self.cmd = cmd
+        self.t = t
+        self.x = x
+        self.name = name
+        self.n = n
 
-def table_spin(l):
-    global table
-    global customer_remain
-    for k,v in table.items():
-        for i in range(len(v)):
-            if v[i] == l-1:
-                v[i] = 0
-            else:
-                v[i] += 1
-    # 돌아온 초밥 계산
-    copy_table = copy.deepcopy(table)
-    for k,v in copy_table.items():
-        for i in v:
-            if customer_remain.get((i, k), 0) != 0 and customer_remain[(i, k)] >= 1:
-                customer_remain[(i, k)] -= 1
-                table[k].remove(i)
-                if customer_remain[(i, k)] == 0:
-                    del customer_remain[(i, k)]
+# 명령들을 관리합니다.
+queries = []
 
-def sushi(temp):
-    global table
-    table_num = int(temp[2])
-    sushi_name = temp[3]
-    table[sushi_name].append(table_num)
-    # 새로 놓인 초밥
-    copy_table = copy.deepcopy(table)
-    if customer_remain.get((table_num, sushi_name), 0) != 0:
-        for v in copy_table[sushi_name]:
-            if v == table_num and customer_remain[(table_num, sushi_name)] >= 1:
-                customer_remain[(table_num, sushi_name)] -= 1
-                table[sushi_name].remove(v)
-        if customer_remain[(table_num, sushi_name)] == 0:
-            del customer_remain[(table_num, sushi_name)]
+# 등장한 사람 목록을 관리합니다.
+names = set()
 
+# 각 사람마다 주어진 초밥 명령만을 관리합니다.
+p_queries = {}
 
-def customer(temp):
-    global table
-    global customer_remain
-    table_num = int(temp[2])
-    customer_name = temp[3]
-    should_eat = int(temp[4])
-    customer_remain[(table_num, customer_name)] = should_eat
-    copy_table = copy.deepcopy(table)
-    if table[customer_name] and table_num in table[customer_name]:
-        for v in copy_table[customer_name]:
-            if v == table_num and customer_remain[(table_num, customer_name)] >= 1:
-                customer_remain[(table_num, customer_name)] -= 1
-                table[customer_name].remove(v)
-    if customer_remain[(table_num, customer_name)] == 0:
-        del customer_remain[(table_num, customer_name)]
+# 각 사람마다 입장 시간을 관리합니다.
+entry_time = {}
 
+# 각 손님의 위치를 관리합니다.
+position = {}
 
-def picture():
-    global table
-    global customer_remain
-    count_sushi = 0
-    count_customer = 0
-    for k in table.keys():
-        if table[k]:
-            count_sushi += len(table[k])
-    count_customer += len(customer_remain)
-    return count_customer, count_sushi
+# 각 사람마다 퇴장 시간을 관리합니다.
+exit_time = {}
 
+# Query를 (t, cmd) 순으로 정렬합니다.
+def cmp(q1, q2):
+    if q1.t != q2.t:
+        return q1.t < q2.t
+    return q1.cmd < q2.cmd
 
-def main():
-    L, Q = map(int, input().split())
-    last_t = 1
-    current_t = 1
-    for i in range(Q):
-        temp = list(input().split())
-        current_t = int(temp[1])
-        #print('t:', current_t, temp)
-        for _ in range(current_t - last_t):
-            table_spin(L)
-        last_t = int(temp[1])
-        if temp[0] == '100':
-            sushi(temp)
-            #print('100 table:', table)
-            #print('100 customer:', customer_remain)
-        if temp[0] == '200':
-            customer(temp)
-            #print('200 table:', table)
-            #print('200 customer:', customer_remain)
-        if temp[0] == '300':
-            sushi_left, customer_left = picture()
-            #print('300 table:', table)
-            #print('300 customer:', customer_remain)
-            print(sushi_left, customer_left)
+# 입력:
+L, Q = map(int, input().split())
+for _ in range(Q):
+    command = input().split()
+    cmd, t, x, n = -1, -1, -1, -1
+    name = ""
+    cmd = int(command[0])
+    if cmd == 100:
+        t, x, name = command[1:]
+        t, x = map(int, [t, x])
+    elif cmd == 200:
+        t, x, name, n = command[1:]
+        t, x, n = map(int, [t, x, n])
+    else:
+        t = int(command[1])
 
+    queries.append(Query(cmd, t, x, name, n))
+    
+    # 사람별 주어진 초밥 목록을 관리합니다.
+    if cmd == 100:
+        if name not in p_queries:
+            p_queries[name] = []
+        p_queries[name].append(Query(cmd, t, x, name, n))
+    # 손님이 입장한 시간과 위치를 관리합니다.
+    elif cmd == 200:
+        names.add(name)
+        entry_time[name] = t
+        position[name] = x
 
-if __name__ == '__main__':
-    main()
+# 각 사람마다 자신의 이름이 적힌 조합을 언제 먹게 되는지를 계산하여 해당 정보를 기존 Query에 추가합니다. (111번 쿼리)
+for name in names:
+    # 해당 사람의 퇴장 시간을 관리합니다.
+    # 이는 마지막으로 먹는 초밥 시간 중 가장 늦은 시간이 됩니다.
+    exit_time[name] = 0
+
+    for q in p_queries[name]:
+        # 만약 초밥이 사람이 등장하기 전에 미리 주어진 상황이라면
+        time_to_removed = 0
+        if q.t < entry_time[name]:
+            # entry_time때의 스시 위치를 구합니다.
+            t_sushi_x = (q.x + (entry_time[name] - q.t)) % L
+            # 몇 초가 더 지나야 만나는지를 계산합니다.
+            additionl_time = (position[name] - t_sushi_x + L) % L
+
+            time_to_removed = entry_time[name] + additionl_time
+        # 초밥이 사람이 등장한 이후에 주어졌다면
+        else:
+            # 몇 초가 더 지나야 만나는지를 계산합니다.
+            additionl_time = (position[name] - q.x + L) % L
+            time_to_removed = q.t + additionl_time
+
+        # 초밥이 사라지는 시간 중 가장 늦은 시간을 업데이트 합니다.
+        exit_time[name] = max(exit_time[name], time_to_removed)
+
+        # 초밥이 사라지는 111번 쿼리를 추가합니다.
+        queries.append(Query(111, time_to_removed, -1, name, -1))
+
+# 사람마다 초밥을 마지막으로 먹은 시간 t를 계산하여 그 사람이 해당 t 때 코드트리 오마카세를 떠났다는 Query를 추가합니다. (222번 쿼리)
+for name in names:
+    queries.append(Query(222, exit_time[name], -1, name, -1))
+
+# 전체 Query를 시간순으로 정렬하되 t가 일치한다면 문제 조건상 사진 촬영에 해당하는 300이 가장 늦게 나오도록 cmd 순으로 오름차순 정렬을 합니다.
+# 이후 순서대로 보면서 사람, 초밥 수를 count하다가 300이 나오면 현재 사람, 초밥 수를 출력합니다.
+queries.sort(key=lambda q: (q.t, q.cmd))
+
+people_num, sushi_num = 0, 0
+for i in range(len(queries)):
+    if queries[i].cmd == 100:  # 초밥 추가
+        sushi_num += 1
+    elif queries[i].cmd == 111:  # 초밥 제거
+        sushi_num -= 1
+    elif queries[i].cmd == 200:  # 사람 추가
+        people_num += 1
+    elif queries[i].cmd == 222:  # 사람 제거
+        people_num -= 1
+    else:  # 사진 촬영시 답을 출력하면 됩니다.
+        print(people_num, sushi_num)
