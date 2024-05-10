6982200df3204e66009d26fc74eeb5c1f317e0ae
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-05-11 05:04:31 +0900

    Title: 빙산의 일각, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240511/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240511/빙산의 일각/the-tip-of-the-iceberg.py
@@ -13,19 +13,25 @@ if N <= 2:
 iceburg_sorted = sorted(list(enumerate(iceburg)))
 mid = iceburg[int(len(iceburg) // 2)]
 idx = [idx for idx, value in iceburg_sorted if value == mid][0]
-idxs = [idx-2, idx-1, idx, idx+1, idx+2]
+idxs = [2]
 
 result = []
 for i in idxs:
     count = 0
-    for ice in range(len(iceburg)):
+    for ice in range(0, len(iceburg), 2):
         # 전 얼음이 해수면보다 높고 현 얼음이 해수면과 같거나 작으면 count+1
         if (0 < ice < len(iceburg) - 1) and iceburg[ice] <= iceburg[i] < iceburg[ice - 1]:
             count += 1
-        if ice == len(iceburg)-1 and iceburg[ice] <= iceburg[i] < iceburg[ice - 1]:
-            continue
-        if ice == len(iceburg)-1 and iceburg[ice - 1] <= iceburg[i] < iceburg[ice]:
+        # 현 얼음이 해수면보다 높고 전 얼음이 해수면과 같거나 작으면 count+1
+        if (0 < ice < len(iceburg) - 1) and iceburg[ice - 1] <= iceburg[i] < iceburg[ice]:
             count += 1
+        if len(iceburg) % 2 == 0 and ice == len(iceburg)-2:
+            # 현 얼음이 해수면보다 낮고 다음 얼음은 해수면보다 높을때
+            if iceburg[ice] <= iceburg[i] < iceburg[ice + 1]:
+                count += 1
+            # 현 얼음이 해수면보다 높고 다음 얼음은 해수면보다 낮을때
+            if iceburg[ice+1] <= iceburg[i] < iceburg[ice]:
+                count += 1
     result.append(count)
 
 print(max(result))
