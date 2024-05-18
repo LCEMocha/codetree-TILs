881d86788c31bd97673884497c2d00c714b58cfd
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-05-19 03:44:38 +0900

    Title: Carry 피하기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240519/Carry 피하기/escaping-carry.py
+++ 240519/Carry 피하기/escaping-carry.py
@@ -1,5 +1,9 @@
 N = int(input())
-nums = [int(input().strip()) for _ in range(N)]
+nums = []
+
+for _ in range(N):
+    n = int(input())
+    nums.append(n)
 
 max_count = 0
 
@@ -20,19 +24,16 @@ def is_not_carry(sum, target):
     # 반복문이 정상으로 종료되면 carry가 발생하지 않음
     return True
 
-def backtrack(count, depth, sum):
+def backtrack(current_sum, depth, count):
+    global nums
     global max_count
+    for i in range(depth, len(nums)):
+        if is_not_carry(current_sum, nums[i]):
+            current_sum += nums[i]
+            count += 1
+            max_count = max(count, max_count)
+            backtrack(current_sum, depth+1, count)
 
-    # 이전까지 최대 개수(max_count)와 현재까지 구한 개수(count) 중 큰 값으로 갱신
-    max_count = max(max_count, count)
-
-    # nums 순회
-    for i in range(depth, N):
-        # carry가 발생하지 않았다면
-        if is_not_carry(sum, nums[i]):
-            # count, depth 증가시키고, sum과 nums[i]를 더한 후 backtrack 재귀 호출
-            backtrack(count + 1, i + 1, sum + nums[i])
-
-# 초기 백트래킹 시작
 backtrack(0, 0, 0)
+
 print(max_count)
