0d666d0236e02eef8f6e6949a7f4455af36d21b3
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-08-28 04:54:14 +0900

    Title: 함수 실행 횟수, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240828/함수 실행 횟수/number-of-function-executions.py
+++ 240828/함수 실행 횟수/number-of-function-executions.py
@@ -1,14 +1,20 @@
 n = int(input())
 
-def count(N) :
-    if (N < 2) :
+# 메모이제이션을 위한 배열 초기화
+memo = [-1] * (n + 1)
+
+def count(N):
+    if N == 0 or N == 1:
         return 1
-    else :
-        if N == 2 :
-            return 3
-        if N == 3 :
-            return 5
-        else:
-            return count(N-1) + count(N-2) + 1
+    if N == 2:
+        return 3
+    if N == 3:
+        return 5
+    if memo[N] != -1:
+        return memo[N]  # 이미 계산된 값이 있으면 그 값을 반환
+    else:
+        # 값을 계산하고 메모 배열에 저장
+        memo[N] = count(N-1) + count(N-2) + 1
+        return memo[N]
 
 print(count(n)%1000000007)
