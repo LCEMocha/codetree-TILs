c6f8b5fb58d3b1793ee0a1f2b40f4f8a3fe0d51b
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-08-28 04:17:25 +0900

    Title: 함수 실행 횟수, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240828/함수 실행 횟수/number-of-function-executions.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+def count(N) :
+    if (N < 2) :
+        return 1
+    else :
+        if N == 2 :
+            return 3
+        if N == 3 :
+            return 5
+        else:
+            return count(N-1) + count(N-2)
+
+print(count(n))
