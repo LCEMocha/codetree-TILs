be414942d54de2d7a12dda11cb1e8f9ed5549607
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-08-31 21:29:58 +0900

    Title: 연속되는 수, Time: 398ms, Memory: 26MB, Status: Passed - CodeTree

--- 240831/연속되는 수/continuous-number.py
+++ 240831/연속되는 수/continuous-number.py
@@ -7,9 +7,10 @@ for i in deDuplicate:
     filtered_numbers = [x for x in numbers if x != i]
     if not filtered_numbers:
         max_continue = [0, 0]
+    
     curr_continue = [[0, 0] for _ in range(len(deDuplicate))]
     curr_continue_count = 0
-    for j in range(len(filtered_numbers)):       
+    for j in range(len(filtered_numbers)):     
         if j+1<=len(filtered_numbers)-1 and filtered_numbers[j] == filtered_numbers[j+1]:
             curr_continue_count += 1
         elif j>0 and filtered_numbers[j] == filtered_numbers[j-1]:
@@ -18,10 +19,10 @@ for i in deDuplicate:
             if curr_continue[index][0] < curr_continue_count:
                 curr_continue[index][1] = [filtered_numbers[j]]
                 curr_continue[index][0] = curr_continue_count
+            curr_continue_count = 0
         else:
             curr_continue_count = 0
             continue
-   
     max_curr = max(curr_continue)
     max_continue = max(max_continue, max_curr)
 
