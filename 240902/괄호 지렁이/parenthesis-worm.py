7fa0278bf9fce6c02c41c1ae2efff9982207f9a2
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-09-02 05:20:57 +0900

    Title: 괄호 지렁이, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240902/괄호 지렁이/parenthesis-worm.py
+++ 240902/괄호 지렁이/parenthesis-worm.py
@@ -28,13 +28,11 @@ def backtrack(grid, N, visited, curr_len, x, y):
     for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
         nx, ny = x + dx, y + dy
         if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
-            if grid[x][y] == ')' and grid[nx][ny] == '(':
-                visited[x][y] = False
-                return
-            else:
+            # grid[x][y]가 ')'이고, grid[nx][ny]가 '('이면 이동하지 않음
+            if not (grid[x][y] == ')' and grid[nx][ny] == '('):
                 backtrack(grid, N, visited, curr_len + 1, nx, ny)
     visited[x][y] = False  # 상태 복구
 
 backtrack(grid, N, visited, 1, 0, 0)
 
-print(max_len+1)
+print(max_len)
