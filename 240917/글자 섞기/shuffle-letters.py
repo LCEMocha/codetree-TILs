e347b7c497de33bee6454cae491ab34829905a84
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-09-17 02:20:37 +0900

    Title: 글자 섞기, Time: 1299ms, Memory: 46MB, Status: Passed - CodeTree

--- 240917/글자 섞기/shuffle-letters.py
+++ 240917/글자 섞기/shuffle-letters.py
@@ -17,12 +17,15 @@ last.sort()
 first_positions = [0] * N
 last_positions = [0] * N
 
+sorted_first_strings = [x[0] for x in first]
+sorted_last_strings = [x[0] for x in last]
+
 # 이진탐색
 for i in range(N):
     # 빠른순서 정렬
-    first_positions[first[i][1]] = bisect.bisect_left([x[0] for x in last], first[i][0])
+    first_positions[first[i][1]] = bisect.bisect_left(sorted_last_strings, first[i][0])
     # 느린 순서 정렬
-    last_positions[last[i][1]] = bisect.bisect_right([x[0] for x in first], last[i][0])
+    last_positions[last[i][1]] = bisect.bisect_right(sorted_first_strings, last[i][0])
 
 for i in range(N):
-    print(first_positions[i] + 1, last_positions[i])
+    print(first_positions[i] + 1, last_positions[i]) # bisect는 삽입순서를 찾는 모듈이라 last에는 1을 +하지 말아야함
