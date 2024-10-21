106f3691878bdb326d3cd512a4649ec3f88e827c
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-10-21 22:34:31 +0900

    Title: 루프 탈출, Time: 194ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 241021/루프 탈출/loop-escape.py
+++ 241021/루프 탈출/loop-escape.py
@@ -14,14 +14,18 @@ def dfs(k, current_count, copy_numberes, current_visit):
     current_visit.append(k)
     if copy_numberes[k]:  
         next_value = copy_numberes[k].pop()
-        if next_value == 0 and current_count == 0:
+        if next_value == 0 and current_count == 1:
+            count += current_count
             flag = False
             return flag
         if next_value in current_visit:
             flag = False
             return flag
         if next_value == 0 or not copy_numberes[next_value]:
-            count += (current_count + 1)
+            if next_value == 0:
+                count += current_count -1
+            else:
+                count += current_count
             flag = False
             return flag
         if next_value != 0 and next_value not in current_visit:
@@ -32,6 +36,6 @@ for k in numbers.keys():
     flag = True
     copy_numbers = copy.deepcopy(numbers)
     while flag :
-        dfs(k, 0, copy_numbers, [])
+        dfs(k, 1, copy_numbers, [])
     
 print(count)
